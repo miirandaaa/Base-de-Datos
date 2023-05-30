6df9cc045e6226b31016f95b10ccc07cf7678131
miirandaaa <131990078+miirandaaa@users.noreply.github.com>
2023-05-30 12:37:00 -0300

    update main

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def create_tables():
 if __name__ == '__main__':
    estado = True
    while estado:
-      opcion = int(input(print ("\n1 Ingresar Datos \n2 Modifcar Datos \n3 Eliminar Datos \n4 Consultar Datos \n5 Salir")))
+      opcion = int(input("\n1 Ingresar Datos \n2 Modifcar Datos \n3 Eliminar Datos \n4 Consultar Datos \n5 Salir \nOpcion: "))
       if opcion == 1:
-         ingresar = int(input(print("\n1 Ingresar Persona \n2 Ingrsar Propietario \n3 Ingresar Cuenta \n4 Ingresar Vehiculo \n5 Ingresar Peaje \n6 Ingresar Ventanilla")))
+         ingresar = int(input("\n1 Ingresar Persona \n2 Ingrsar Propietario \n3 Ingresar Cuenta \n4 Ingresar Vehiculo \n5 Ingresar Peaje \n6 Ingresar Ventanilla \nOpcion: "))
       if opcion == 5:
          estado = False
 
