7d0739e733e54223f3df90e5a0b3fcd2eb0564ec
miirandaaa <131990078+miirandaaa@users.noreply.github.com>
2023-05-30 20:20:51 -0300

    sape

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from Peaje import *
 def create_tables():
    psql_db.create_tables([peaje,persona,propietario,propietario_tiene_vehiculo,vehiculo,ventanilla,empresa,tipo_vehiculo,tarifa,cuenta,persona_pariente,credito,bonificacion,debito,cuenta_propietario])
 
-db_connect()
-create_tables()
+#db_connect()
+#create_tables()
 
 if __name__ == '__main__':
    estado = True
