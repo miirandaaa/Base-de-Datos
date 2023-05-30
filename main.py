f682498024cb66f773c1511ee31434045190cadb
miirandaaa <131990078+miirandaaa@users.noreply.github.com>
2023-05-30 11:17:41 -0300

    creando main

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ from Tablas import *
 def create_tables():
    psql_db.create_tables([peaje,persona,propietario,propietario_tiene_vehiculo,vehiculo,ventanilla,empresa,tipo_vehiculo,tarifa,cuenta,persona_pariente,credito,bonificacion,debito,cuenta_propietario])
 
-db_connect()
-create_tables()
+# db_connect()
+# create_tables()
 
 if __name__ == '__main__':
-   opcion = print ("Ingrese una opcion: nl/ Ingresar ")
+   estado = True
+   while estado:
+      opcion = int(input(print ("\n1 Ingresar Datos \n2 Modifcar Datos \n3 Eliminar Datos \n4 Consultar Datos \n5 Salir")))
+      if opcion == 1:
+         ingresar = int(input(print("\n1 Ingresar Persona \n2 Ingrsar Propietario \n3 Ingresar Cuenta \n4 Ingresar Vehiculo \n5 Ingresar Peaje \n6 Ingresar Ventanilla")))
+      if opcion == 5:
+         estado = False
+
+      
+# - Personas
+# - Propietarios
+# - Cuentas
+# - Vehículos
+# - Peajes
+# - Ventanillas
