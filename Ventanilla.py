d5bab8da188b9b682d35687bb3267b2220c12ab0
miirandaaa <131990078+miirandaaa@users.noreply.github.com>
2023-05-31 12:02:41 -0300

    ventanilla

--- Ventanilla.py
+++ Ventanilla.py
@@ -7,7 +7,7 @@ def ingresar_ventanilla(nombre_p, numero_ventanilla, tiene_rfid):
     with psql_db.atomic():
         peaje_obj = peaje.get_or_none(peaje.nombre == nombre_p)
         if peaje_obj:
-            ventanilla.create(nombre_peaje=peaje, nro=numero_ventanilla, tiene_rfid=tiene_rfid)
+            ventanilla.create(nombre_peaje=nombre_p, nro=numero_ventanilla, tiene_rfid=tiene_rfid)
             print("Ventanilla creada correctamente.")
         else:
             print("El peaje especificado no existe.")
