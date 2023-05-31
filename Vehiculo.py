5b4837a248184baa7f9d0281a747d48fd97db758
miirandaaa <131990078+miirandaaa@users.noreply.github.com>
2023-05-31 12:25:22 -0300

    vehiculo terminado

--- Vehiculo.py
+++ Vehiculo.py
@@ -6,12 +6,14 @@ from Peaje import *
 def ingresar_vehiculo(matricula, tag_rfid, marca, color, tipo):
      with psql_db.atomic():
         try:
-            if vehiculo.get_or_none(vehiculo.matricuala == matricula):
+            if vehiculo.get_or_none(vehiculo.matricula == matricula):
                 print("El vehiculo ingresado ya existe.")
             else:
                 tipo_existe = tipo_vehiculo.get_or_none(tipo_vehiculo.tipo == tipo)
                 if tipo_existe:
                     vehiculo.create(matricula=matricula, tag_rfid=tag_rfid, marca=marca, color=color, tipo_vehiculo=tipo)
                     print("Vehiculo creado correctamente.")
+                else:
+                    print("El tipo de vehiculo ingresado no existe.")
         except IntegrityError:
-            print("Error: No se pudo crear el peaje debido a una violación de restricción única.")
+            print("Error: No se pudo crear el vehiculo debido a una violación de restricción única.")
