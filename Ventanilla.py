a8f44b67ff5c51ecc222c50494edbd04aff1fcca
miirandaaa <131990078+miirandaaa@users.noreply.github.com>
2023-05-31 12:03:32 -0300

    vnetnailla

--- Ventanilla.py
+++ Ventanilla.py
@@ -5,9 +5,12 @@ from Peaje import *
 
 def ingresar_ventanilla(nombre_p, numero_ventanilla, tiene_rfid):
     with psql_db.atomic():
-        peaje_obj = peaje.get_or_none(peaje.nombre == nombre_p)
-        if peaje_obj:
-            ventanilla.create(nombre_peaje=nombre_p, nro=numero_ventanilla, tiene_rfid=tiene_rfid)
-            print("Ventanilla creada correctamente.")
-        else:
-            print("El peaje especificado no existe.")
+        try:
+            peaje_obj = peaje.get_or_none(peaje.nombre == nombre_p)
+            if peaje_obj:
+                ventanilla.create(nombre_peaje=nombre_p, nro=numero_ventanilla, tiene_rfid=tiene_rfid)
+                print("Ventanilla creada correctamente.")
+            else:
+                print("El peaje especificado no existe.")
+        except IntegrityError:
+            print("Error: No se pudo crear la ventanilla debido a una violación de restricción única.")
