a6e5bc59d1efa69e2c94611a30e808c3be73e4b3
miirandaaa <131990078+miirandaaa@users.noreply.github.com>
2023-06-02 11:33:53 -0300

    god

--- Propietario.py
+++ Propietario.py
@@ -6,10 +6,10 @@ from peewee import *
 def ingresar_propietario(dni, nombres, apellidos, celular, email, direccion, matricula, tag_rfid, marca, modelo, color, tipo):
     with psql_db.atomic():
         try:
-            nuevo_prop = propietario.create(tipo_propietario="Persona")
             if persona.get_or_none(persona.dni == dni) and vehiculo.get_or_none(vehiculo.matricula == matricula):
                 print("El Propietario y el Vehiculo ingresados ya existen.")
             else:
+                nuevo_prop = propietario.create(tipo_propietario="Persona")
                 persona.create(dni=dni, id_propietario=nuevo_prop.id_propietario, nombres=nombres, apellidos=apellidos, celular=celular, email=email, direccion=direccion)
                 vehiculo.create(matricula=matricula, tag_rfid=tag_rfid, marca=marca, modelo=modelo, color=color, tipo_vehiculo=tipo, id_propietario=nuevo_prop.id_propietario)
                 print("Propietario y Vehiculo agregado correctamente.")
