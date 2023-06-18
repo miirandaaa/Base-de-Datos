6e3ee1bc65edcd8732afcd9acf803854a7153fbb
miirandaaa <131990078+miirandaaa@users.noreply.github.com>
2023-06-18 20:58:08 -0300

    bien

--- Vehiculo.py
+++ Vehiculo.py
@@ -2,19 +2,23 @@ from Tablas import *
 from Config import *
 from Peaje import *
 
-def ingresar_vehiculo(matricula, tag_rfid, marca, modelo, color, tipo):
+def ingresar_vehiculo(dni, matricula, tag_rfid, marca, modelo, color, tipo):
      with psql_db.atomic():
         try:
             if vehiculo.get_or_none(vehiculo.matricula == matricula):
                 print("El vehiculo ingresado ya existe.")
             else:
-                tipo_existe = tipo_vehiculo.get_or_none(tipo_vehiculo.tipo == tipo)
-                if tipo_existe:
-                    vehiculo.create(matricula=matricula, tag_rfid=tag_rfid, marca=marca, modelo=modelo, color=color, tipo_vehiculo=tipo)
-                    psql_db.commit()
-                    print("Vehiculo creado correctamente.")
-                else:
-                    print("El tipo de vehiculo ingresado no existe.")
+                dueño = propietario.get_or_none(propietario.dni == dni)
+                if dueño:
+                    tipo_existe = tipo_vehiculo.get_or_none(tipo_vehiculo.tipo == tipo)
+                    if tipo_existe:
+                        dueño_id = dueño.id_propietario
+                        propietario_tiene_vehiculo.create(id_propietario=dueño_id, matricula=matricula)
+                        vehiculo.create(matricula=matricula, tag_rfid=tag_rfid, marca=marca, modelo=modelo, color=color, tipo_vehiculo=tipo)
+                        psql_db.commit()
+                        print("Vehiculo creado correctamente.")
+                    else:
+                        print("El tipo de vehiculo ingresado no existe.")
         except IntegrityError():
             psql_db.rollback()
             print("Error: No se pudo crear el vehiculo debido a una violación de restricción única.")
@@ -49,7 +53,7 @@ def consultar_vehiuclo(vehiculo_aconsultar):
             print(f"Matricula: {vehiculo_querido.matricula} \nTag RFID: {vehiculo_querido.tag_rfid} \nMarca: {vehiculo_querido.marca} \nModelo: {vehiculo_querido.modelo} \nColor: {vehiculo_querido.color} \nTipo Vehiculo: {vehiculo_querido.tipo_vehiculo}")
         except IntegrityError():
             print("Error: No se pudo consultar el vehiculo debido a una violación de restricción única.")
-            
+
 def eliminar_vehiculo(vehiculo_aeliminar):
     with psql_db.atomic():
         propietario_querido = propietario_tiene_vehiculo.get_by_id(vehiculo_aeliminar)
