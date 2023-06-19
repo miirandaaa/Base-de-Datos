0790bbf1e7a9573c92af5a151daeece2f5de6931
miirandaaa <131990078+miirandaaa@users.noreply.github.com>
2023-06-19 15:30:57 -0300

    consulta 1

--- Reportes.py
+++ Reportes.py
@@ -9,7 +9,11 @@ def reporte_titular_y_vehiculos():
             .join(persona, on=(persona.id_propietario == propietario_tiene_vehiculo.id_propietario))
             .dicts())
 
-        results = reportes.execute()
-        for row in results:
-            print(f"Propietario: {row.get('dni')} {row.get('nombres')} {row.get('apellidos')} \nVehiculo: {row.get('matricula')} {row.get('marca')} {row.get('modelo')} {row.get('color')} {row.get('tipo_vehiculo')}")
-            print(row)
+        resultado = reportes.execute()
+        fila_pasada = None
+        for fila in resultado:
+            if fila_pasada is None or fila.get('id_propietario') != fila_pasada.get('id_propietario'):
+                print(f"Propietario: {fila.get('dni')} {fila.get('nombres')} {fila.get('apellidos')} {fila.get('celular')} {fila.get('direccion')} \nVehiculo: {fila.get('matricula')} {fila.get('marca')} {fila.get('modelo')} {fila.get('color')}")
+                fila_pasada = fila
+            else:
+                print(f"Vehiculo: {fila.get('matricula')} {fila.get('marca')} {fila.get('modelo')} {fila.get('color')}")
