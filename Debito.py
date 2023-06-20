7535dc809bde4e7e2021519a3ee873052000be44
ramiromartinezum <rmartinez5@correo.um.edu.uy>
2023-06-20 03:27:15 -0300

    EL PUSH

--- Debito.py
+++ Debito.py
@@ -19,10 +19,18 @@ def debitar():
                 if check:
                     vehiculo_pasada = vehiculo.get_by_id(matricula)
                     tarifa_pasada = tarifa.select(tarifa.valor).where((tarifa.tipo_vehiculo == vehiculo_pasada.tipo_vehiculo) & (tarifa.fecha_vigencia <= fecha_actual)).limit(1)
-                    cuenta_a_debitar = (propietario_tiene_vehiculo.select(cuenta.nro_cuenta).where(propietario_tiene_vehiculo.matricula == matricula).join(cuenta, on=(propietario_tiene_vehiculo.id_propietario == cuenta.id_propietario)))
-                    debito.create(matricula = matricula,id_ventanilla = vent.id_ventanilla, fecha_hora_debito = fecha_hora_actual, importe_debito = tarifa_pasada, numero_cuenta = cuenta_a_debitar)
                     
-                    cuenta_debito = cuenta.get_by_id(cuenta_a_debitar)
+                    dni_prop = persona.select(persona.dni).where(propietario_tiene_vehiculo.matricula == matricula).join(propietario_tiene_vehiculo, on=(persona.id_propietario == propietario_tiene_vehiculo.id_propietario)) 
+                    dni_pariente = persona_pariente.select(persona_pariente.dni_pariente).where(persona_pariente.dni == dni_prop)
+
+                    if dni_pariente.scalar() is None:
+                        cuenta_a_debitar = (propietario_tiene_vehiculo.select(cuenta.nro_cuenta).where(propietario_tiene_vehiculo.matricula == matricula).join(cuenta, on=(propietario_tiene_vehiculo.id_propietario == cuenta.id_propietario)))
+                        cuenta_debito = cuenta.get_by_id(cuenta_a_debitar)
+                    else:
+                        jefe = persona.get_by_id(dni_pariente)
+                        cuenta_debito = cuenta.get(id_propietario = jefe.id_propietario)
+                    
+                    debito.create(matricula = matricula,id_ventanilla = vent.id_ventanilla, fecha_hora_debito = fecha_hora_actual, importe_debito = tarifa_pasada, numero_cuenta = cuenta_debito.nro_cuenta)
                     saldo_pasado = cuenta_debito.saldo
                     monto_debitar = tarifa_pasada.scalar()
                     nuevo_saldo = int(saldo_pasado - monto_debitar)
