0c937a8ed27583d7b820e2c0f0b7baaab8e92ca3
ramiromartinezum <rmartinez5@correo.um.edu.uy>
2023-06-19 23:56:32 -0300

    ultimos

--- Debito.py
+++ Debito.py
@@ -2,23 +2,27 @@ from Tablas import *
 from Config import *
 from Cuenta import *
 from datetime import datetime
-import datetime
 
 def debito():
     with db_conn['rdbms'].atomic():
         try:
             fecha_hora_actual = datetime.now()
-            fecha_actual = datetime.date(fecha_hora_actual.year,fecha_hora_actual.month,fecha_hora_actual.day,0,0)
+            fecha_actual = datetime(fecha_hora_actual.year,fecha_hora_actual.month,fecha_hora_actual.day,0,0)
             bonificacion = 10
-            matricula = int(input('Ingrese matricula del vehiculo'))
-            if vehiculo.get_or_none(vehiculo.matricula == matricula):
-                vehiculo_pasada = vehiculo.get_by_id(vehiculo.matricula == matricula)
-                tarifa_pasada = tarifa.select(tarifa.valor).where(tarifa.tipo_vehiculo == vehiculo_pasada.tipo_vehiculo and tarifa.fecha_vigencia >= fecha_actual)
-                print(tarifa_pasada)
+            matricula = int(input('Ingrese matricula del vehiculo '))
+            id_peaje = int(input('Ingrese id peaje '))
+            nro_ventanilla = int(input('Ingrese numero ventanilla '))
 
-                cuenta_debitar = cuenta.join(propietario_tiene_vehiculo, on=(propietario_tiene_vehiculo.id_propietario == cuenta.id_propietario)).join(vehiculo, on=(vehiculo.matricula == matricula, propietario_tiene_vehiculo.matricula == vehiculo.matricula)).select(cuenta.nro_cuenta)
-                print(cuenta_debitar)
-                print('Debito exitoso.')
+            if vehiculo.get_or_none(vehiculo.matricula == matricula):
+                if ventanilla.get_or_none(ventanilla.id_peaje == id_peaje, ventanilla.nro_ventanilla == nro_ventanilla):
+                    ventanilla_id = ventanilla.get_by_id(id_peaje,nro_ventanilla)
+                    vehiculo_pasada = vehiculo.get_by_id(vehiculo.matricula == matricula)
+                    tarifa_pasada = tarifa.select(tarifa.valor).where(tarifa.tipo_vehiculo == vehiculo_pasada.tipo_vehiculo and tarifa.fecha_vigencia <= fecha_actual)
+                    debito.create(matricula = matricula, id_ventanilla = ventanilla_id, fehca_hora_debito = fecha_hora_actual, importe = tarifa_pasada)
+                    #cuenta_debitar = cuenta.join(propietario_tiene_vehiculo, on=(propietario_tiene_vehiculo.id_propietario == cuenta.id_propietario)).join(vehiculo, on=(vehiculo.matricula == matricula, propietario_tiene_vehiculo.matricula == vehiculo.matricula)).select(cuenta.nro_cuenta)
+                    
+                    db_conn['rdbms'].commit()
+                    print('Debito exitoso.')
 
         except IntegrityError():
             db_conn['rdbms'].rollback()
